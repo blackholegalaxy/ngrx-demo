deac7088f5f5c6ad44c9e120fd0b1f43b9878565
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2017-12-16 17:08:02 -0500

    feat: create getSelectedPizza selector using router state

--- src/products/store/selectors/pizzas.selectors.ts
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -4,6 +4,8 @@ import * as fromRoot from '../../../app/store';
 import * as fromFeature from '../reducers';
 import * as fromPizzas from '../reducers/pizzas.reducer';
 
+import { Pizza } from './../../models/pizza.model';
+
 export const getPizzaState = createSelector(
   fromFeature.getProductsState,
   (state: fromFeature.ProductsState) => state.pizzas
@@ -18,6 +20,14 @@ export const getAllPizzas = createSelector(
   getPizzasEntities,
   (entities) => {
     return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
+  }
+)
+
+export const getSelectedPizza = createSelector(
+  getPizzasEntities,
+  fromRoot.getRouterState,
+  (entities, router): Pizza => {
+    return router.state && entities[router.state.params.pizzaId];
   }
 )
 
