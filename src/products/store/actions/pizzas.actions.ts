2d45dd8f997f3183eb56d780677bce77ae1f5e3a
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2017-12-18 18:43:56 -0500

    feat: create a pizza entity

--- src/products/store/actions/pizzas.actions.ts
+++ src/products/store/actions/pizzas.actions.ts
@@ -2,7 +2,7 @@ import { Action } from '@ngrx/store';
 
 import { Pizza } from '../../models/pizza.model';
 
-// load pizzas
+// Load pizzas
 export const LOAD_PIZZAS = '[Products] Load Pizzas'
 export const LOAD_PIZZAS_FAIL = '[Products] Load Pizzas Fail'
 export const LOAD_PIZZAS_SUCCESS = '[Products] Load Pizzas Success'
@@ -21,5 +21,31 @@ export class LoadPizzasSuccess implements Action {
   constructor(public payload: Pizza[]) { }
 }
 
+// Create pizzas
+export const CREATE_PIZZA = '[Products] Create pizza';
+export const CREATE_PIZZA_FAIL = '[Products] Create pizza fail';
+export const CREATE_PIZZA_SUCCESS = '[Products] Create pizza success';
+
+export class CreatePizza implements Action {
+  readonly type = CREATE_PIZZA;
+  constructor(public payload: Pizza) { }
+}
+
+export class CreatePizzaFail implements Action {
+  readonly type = CREATE_PIZZA_FAIL;
+  constructor(public payload: any) { }
+}
+
+export class CreatePizzaSuccess implements Action {
+  readonly type = CREATE_PIZZA_SUCCESS;
+  constructor(public payload: Pizza) { }
+}
+
 // action types
-export type PizzasAction = LoadPizzas | LoadPizzasFail | LoadPizzasSuccess;
+export type PizzasAction =
+  | LoadPizzas
+  | LoadPizzasFail
+  | LoadPizzasSuccess
+  | CreatePizza
+  | CreatePizzaFail
+  | CreatePizzaSuccess;
--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -12,11 +12,21 @@ export class PizzasEffects {
   constructor(private actions$: Actions, private pizzasService: fromServices.PizzasService) { }
 
   @Effect() // Option: {dispatch: false} to prevent dispatch action after effect
-  loadPizzas$ = this.actions$.ofType(pizzasActions.LOAD_PIZZAS)
-    .pipe(switchMap(() => {
+  loadPizzas$ = this.actions$.ofType(pizzasActions.LOAD_PIZZAS).pipe(
+    switchMap(() => {
       return this.pizzasService.getPizzas().pipe(
         map(pizzas => new pizzasActions.LoadPizzasSuccess(pizzas)),
         catchError(error => of(new pizzasActions.LoadPizzasFail(error)))
+      );
+    }));
+
+  @Effect()
+  createPizza$ = this.actions$.ofType(pizzasActions.CREATE_PIZZA).pipe(
+    map((action: pizzasActions.CreatePizza) => action.payload),
+    switchMap(pizza => {
+      return this.pizzasService.createPizza(pizza).pipe(
+        map(pizza => new pizzasActions.CreatePizzaSuccess(pizza)),
+        catchError(error => of(new pizzasActions.CreatePizzaFail(error)))
       );
     }));
 }
