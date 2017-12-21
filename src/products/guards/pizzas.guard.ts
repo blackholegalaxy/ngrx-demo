04fdc0632849eb46336ec348330ba18e5e167044
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2017-12-21 16:05:52 -0500

    refact: export checkPizzaLoaded guard util

--- src/products/guards/pizzas.guard.ts
+++ src/products/guards/pizzas.guard.ts
@@ -3,12 +3,12 @@ import { CanActivate } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/Observable/of';
-import { tap, filter, take, switchMap, catchError } from 'rxjs/operators';
+import { switchMap, catchError } from 'rxjs/operators';
 
 import { Store } from '@ngrx/store';
 
 import * as fromStore from '../store';
-
+import * as fromGuardUtils from './utils';
 
 @Injectable()
 export class PizzasGuard implements CanActivate {
@@ -23,14 +23,6 @@ export class PizzasGuard implements CanActivate {
   }
 
   checkStore(): Observable<boolean> {
-    return this.store.select(fromStore.getPizzasLoaded).pipe(
-      tap(loaded => {
-        if (!loaded) {
-          this.store.dispatch(new fromStore.LoadPizzas());
-        }
-      }),
-      filter(loaded => loaded), // Only occurs if loaded is true because tap won't continue stream if not resolved
-      take(1)
-    );
+    return fromGuardUtils.checkPizzaLoaded(this.store);
   }
 }
---
+++ src/products/guards/utils/check-store.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs/Observable';
+import { tap, filter, take } from 'rxjs/operators';
+
+import * as fromStore from '../../store';
+
+import { Store } from '@ngrx/store';
+
+export const checkPizzaLoaded = (store: Store<fromStore.ProductsState>): Observable<boolean> => {
+  return store.select(fromStore.getPizzasLoaded).pipe(
+    tap(loaded => {
+      if (!loaded) {
+        store.dispatch(new fromStore.LoadPizzas());
+      }
+    }),
+    filter(loaded => loaded),
+    take(1)
+  );
+};
